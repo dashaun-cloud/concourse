c04323826939a7eb7d4ab24acffe7a2c92f9f8db
DaShaun Carter <dashaun@dashaun.com>
2019-10-05 00:09:12 -0500

    remove service account and project name, add setup modules

---
+++ platformautomation-setup/gcp/main.tf
@@ -0,0 +1,15 @@
+provider "google" {
+  project     = var.project
+  region      = var.region
+  credentials = var.service_account_key
+
+  version = "~> 2.5"
+}
+
+terraform {
+  required_version = ">= 0.12.0"
+}
+
+resource "google_storage_bucket" "platform-automation" {
+  name     = "platform-automation-bucket"
+}
