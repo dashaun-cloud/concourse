9a72b9954027bb9de7d99a85a71a177880faa86b
DaShaun Carter <dashaun@dashaun.com>
2019-10-05 18:37:46 -0500

    Add outputs for foundation-code

--- platformautomation-setup/gcp/outputs.tf
+++ platformautomation-setup/gcp/outputs.tf
@@ -1,7 +1,15 @@
-output "url" {
+output "platform-automation.url" {
   value = google_storage_bucket.platform-automation.url
 }
 
-output "self_link" {
+output "platform-automation.self_link" {
   value = google_storage_bucket.platform-automation.self_link
+}
+
+output "foundation-code.url" {
+  value = google_storage_bucket.foundation-code.url
+}
+
+output "foundation-code.self_link" {
+  value = google_storage_bucket.foundation-code.self_link
 }
