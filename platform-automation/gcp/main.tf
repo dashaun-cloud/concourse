d504c2b9bcdf4e865d208b7fd5479f824b0e9034
DaShaun Carter <dashaun@dashaun.com>
2019-10-05 19:23:03 -0500

    use location instead of regional

--- platform-automation/gcp/main.tf
+++ platform-automation/gcp/main.tf
@@ -12,10 +12,10 @@ terraform {
 
 resource "google_storage_bucket" "platform-automation" {
   name     = "platform-automation-bucket-dashaun-cloud"
-  storage_class = "REGIONAL"
+  location = var.region
 }
 
 resource "google_storage_bucket" "foundation-code" {
   name     = "foundation-code-bucket-dashaun-cloud"
-  storage_class = "REGIONAL"
+  location = var.region
 }
