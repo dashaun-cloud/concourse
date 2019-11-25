238bdbc6be2522c70208f643ac3688fbbf8e8e62
DaShaun Carter <dashaun@dashaun.com>
2019-11-25 17:40:28 -0600

    test

--- paving-pas/gcp/overrides/jumpbox_override.tf
+++ paving-pas/gcp/overrides/jumpbox_override.tf
@@ -1,5 +1,5 @@
-resource "google_compute_instance" "jumpbox" {
-  scratch_disk {
-    interface : "SCSI"
-  }
-}
+ resource "google_compute_instance" "jumpbox" {
+   scratch_disk {
+     interface = "SCSI"
+   }
+ }
