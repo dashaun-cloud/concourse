57c729f9df2eb27de22ff98d178c1c11e102f3ed
DaShaun Carter <dashaun@dashaun.com>
2019-11-25 22:47:54 -0600

    just outputs

--- paving-pas/gcp/overrides/storage_override.tf
+++ paving-pas/gcp/overrides/storage_override.tf
@@ -1,21 +1,6 @@
-resource "google_storage_bucket" "automation" {
-  name          = "${var.project}-${var.env_name}-automation"
-  location      = var.buckets_location
-  force_destroy = true
-  count         = var.create_gcs_buckets ? 1 : 0
+output "resources_bucket_url" {
+  value = google_storage_bucket.resources.url
 }
-
-resource "google_storage_bucket" "installations" {
-  name          = "${var.project}-${var.env_name}-installations"
-  location      = var.buckets_location
-  force_destroy = true
-  count         = var.create_gcs_buckets ? 1 : 0
-}
-
-output "automation_bucket" {
-  value = element(concat(google_storage_bucket.automation.*.name, [""]), 0)
-}
-
-output "installations_bucket" {
-  value = element(concat(google_storage_bucket.installations.*.name, [""]), 0)
+output "backups_bucket_url" {
+  value = google_storage_bucket.backups.url
 }
